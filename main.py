f407584af37912e1b117a1408c1efd73ddd9d764
Qiushi Yan <qiushi.yann@gmail.com>
2022-11-13 22:28:54 -0600

    add deps

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app", host="0.0.0.0", port=os.environ.get("PORT", 8000), reload=True
+        "main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True
     )
