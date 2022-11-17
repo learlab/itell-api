b094baae5453a57692be677d16a399935e3d0f4e
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2022-11-16 18:20:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import os
 
 from fastapi import FastAPI
 
-from src.model import get_score
+from src.model import get_score, containment_score
 from src.models.summary import SummaryInput
 
 app = FastAPI()
-
 
 @app.get("/")
 def hello():
     return {"message": "Hello World"}
 
-
 @app.post("/score")
 def score(summary_input: SummaryInput):
+#     overlap = containment_score(summary_input.text)
+#     if overlap > .6:
+#         return {"score": 0, "containment": containment}
+#     else:
     return {"score": get_score(summary_input.text)}
 
 
