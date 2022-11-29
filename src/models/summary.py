dd9b203c5eb23940108f7b687265b62cc93ac090
Qiushi Yan <qiushi.yann@gmail.com>
2022-11-29 11:10:39 -0600

    change source to non-optional

--- src/models/summary.py
+++ src/models/summary.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 
+
 class SummaryInput(BaseModel):
-    source: str | None = None
+    source: str
     text: str
 
+
 class SummaryResults(BaseModel):
-    score: float | None = None # deprecated!
+    score: float | None = None  # deprecated!
     content: float | None = None
     wording: float | None = None
     containment: float | None = None
