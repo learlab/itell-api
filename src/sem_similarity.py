40e8d4969532904068aeabf08a3971c184f6cf37
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2022-12-07 12:59:27 -0600

    Update sem_similarity.py
    Removed comments
    
    Caching the stopwords list to improve performance:
    https://stackoverflow.com/questions/19560498/faster-way-to-remove-stop-words-in-python
    
    tokenize_text now uses a list comprehension
    
    renamed "chapter" parameter of get_similarity() to "section"
    
    Added a docstring to get_similarity()

--- src/sem_similarity.py
+++ src/sem_similarity.py
@@ -3,28 +3,27 @@ from nltk.corpus import stopwords
 from gensim.models import Doc2Vec
 from scipy import spatial
 
+# a textbook split into sections
 source_dict_path = '../assets/macroeconomics_2e_section.json'
-model_path = '../assets/doc2vec_model'
 
-# Load the source_dict
+# a custom pre-trained doc2vec model (gensim)
+model_path = '../assets/doc2vec_model' 
+
+english_stop_words = stopwords.words('english')
+
 with open(source_dict_path, "r") as data:
     source_dict = json.loads(data.read())
 
-# Load the model
 model = Doc2Vec.load(model_path)
 
-# A function to tokenize the text
 def tokenize_text(text):
-    tokens = []
-    for word in nltk.word_tokenize(text):
-        if not word in stopwords:
-            tokens.append(word.lower())
-    return tokens
+    return [tok for tok in nltk.word_tokenize(text) if tok not in english_stop_words]
 
-# A function to get the score
-def get_similarity(summary, chapter):
+def get_similarity(summary, section):
+    '''Return semantic similarity score based on summary and source text (section)
+    summary: str
+    section: str
+    '''
     summary_embedding = model.infer_vector(tokenize_text(summary))
-    section_embedding = model.infer_vector(tokenize_text(chapter))
+    section_embedding = model.infer_vector(tokenize_text(section))
     return 1 - spatial.distance.cosine(summary_embedding, section_embedding)
-    
- 
