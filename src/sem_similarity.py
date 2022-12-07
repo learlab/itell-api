f0cbc39eefcb874d216723c691b095988b05aff9
Wesley Morris <82518780+wesleygmorris@users.noreply.github.com>
2022-12-07 11:55:18 -0500

    Update sem_similarity.py
    Changed according to comments
    1) Updated paths for model and dict
    2) Used context manager to open file
    3) Changed function name
    4) Used stop words instead of word length

--- src/sem_similarity.py
+++ src/sem_similarity.py
@@ -3,12 +3,12 @@ from nltk.corpus import stopwords
 from gensim.models import Doc2Vec
 from scipy import spatial
 
-source_dict_path = './assets/macroeconomics_2e_section.json'
-model_path = './assets/doc2vec_model'
+source_dict_path = '../assets/macroeconomics_2e_section.json'
+model_path = '../assets/doc2vec_model'
 
 # Load the source_dict
-data = open(source_dict_path)
-source_dict = json.loads(data.read())
+with open(source_dict_path, "r") as data:
+    source_dict = json.loads(data.read())
 
 # Load the model
 model = Doc2Vec.load(model_path)
@@ -18,13 +18,12 @@ def tokenize_text(text):
     tokens = []
     for sent in nltk.sent_tokenize(text):
         for word in nltk.word_tokenize(sent):
-            if len(word) < 2:
-                continue
-            tokens.append(word.lower())
+            if not word in stopwords:
+                tokens.append(word.lower())
     return tokens
 
 # A function to get the score
-def getSimilarity(summary, chapter):
+def get_similarity(summary, chapter):
     summary_embedding = model.infer_vector(tokenize_text(summary))
     section_embedding = model.infer_vector(tokenize_text(chapter))
     return 1 - spatial.distance.cosine(summary_embedding, section_embedding)
