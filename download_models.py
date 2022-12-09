0f7d4d6db00d4f906f59bff1a662dcb0a19e0fdf
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2022-12-09 14:14:07 -0600

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -2,7 +2,7 @@ import nltk
 from huggingface_hub import hf_hub_download
 
 # These downloads will go to locations that are specific to the current user
-# Make sure not to run as root
+# If running as root, make sure to set the $HOME environment variable
 nltk.download('punkt')
 nltk.download('stopwords')
 hf_hub_download(repo_id='tiedaar/summary-longformer-content', filename='pytorch_model.bin')
