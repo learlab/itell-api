0183649c4dd86851afb76b9e4cf693b9641bad3e
Qiushi Yan <qiushi.yann@gmail.com>
2023-01-10 23:34:10 -0600

    adjust cors

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.summary import SummaryInput, SummaryResults
 from summary_eval import summary_score
-from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "https://textbook-demo.web.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -20,13 +26,15 @@ app.add_middleware(
 def hello():
     return {"message": "Hello World"}
 
+
 @app.post("/score")
 def score(summary_input: SummaryInput) -> SummaryResults:
     return summary_score(summary_input)
+
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True
+        "main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8001)), reload=True
     )
