ea458663165cffc98d0871140136b71030471e9e
Joon Suh Choi <57017670+jchoi92k@users.noreply.github.com>
2023-08-18 12:22:06 -0500

    argument data type specified for AnswerPipeline's process method

--- src/pipelines/answer.py
+++ src/pipelines/answer.py
@@ -5,7 +5,7 @@ class AnswerPipeline():
     def __init__(self):
         self.classifier = pipeline("text-classification", model="facehugger92/POE_QA_mpnetbase")
 
-    def process(self, correct_answer, text_input):
+    def process(self, correct_answer: str, text_input: str):
         modified_text = f"The following two sentences are answers to the same question: {correct_answer}. {text_input}"
         res = self.classifier(modified_text)
         return res[0]
