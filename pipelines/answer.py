80c03bfb9631cbf6055b9865ccc6476a9f2da9b9
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2023-10-02 13:07:00 -0500

    refactor pipelines.answer

--- pipelines/answer.py
+++ pipelines/answer.py
@@ -1,46 +1,50 @@
-'''
-This file creates a class called AnswerPipeline.
-It loads a Bleurt model and an MPnet model for classifying whether an answer matches a correct answer.
-The method process takes a candidate and a reference answer as inputs.
-If both models agree that the candidate is correct, it returns a 2. If both models agree that it is incorrect, it returns a 1.
-If the models disagree it returns a 2.
-'''
+"""
+Defines AnswerPipeline:
+It loads a Bleurt model and an MPnet model for scoring a short answer.
+The __call__ method takes a candidate and a reference answer as inputs.
+If both models agree that the candidate is correct, return 2.
+If the models disagree, return 1.
+If both models agree that it is incorrect, return 0.
+"""
 
 from transformers import pipeline
-from typing import Dict
+
 
 class AnswerPipeline:
+    mpnet_classifier = "tiedaar/short-answer-classification"
+    bleurt_classifier = "vaiibhavgupta/finetuned-bleurt-large"
+    bleurt_threshold = 0.7
+
     def __init__(self):
         self.mpnet_classifier = pipeline(
-            "text-classification", model="tiedaar/short-answer-classification"
+            "text-classification", model=self.mpnet_classifier
         )
         self.bleurt_classifier = pipeline(
-            'text-classification', model="vaiibhavgupta/finetuned-bleurt-large"
+            "text-classification", model=self.bleurt_classifier
         )
-        
-    
-    def process(self, candidate: str, reference: str) -> Dict[str, str]:
-        bleurt_sequence = f'{candidate}[SEP]{reference}'
-        mpnet_sequence = f'{candidate}</s>{reference}'
-        
+
+    def __call__(self, candidate: str, reference: str) -> int:
+        bleurt_sequence = f"{candidate}[SEP]{reference}"
+        mpnet_sequence = f"{candidate}</s>{reference}"
+
         # Get bleurt results
-        bleurt_score = self.bleurt_classifier(bleurt_sequence)[0]['score']
-        if bleurt_score > 0.7:
-            bleurt_res = True
+        bleurt_score = self.bleurt_classifier(bleurt_sequence)[0]["score"]
+        if bleurt_score >= self.bleurt_threshold:
+            bleurt_passing = True
         else:
-            bleurt_res = False
+            bleurt_passing = False
 
         # Get MPnet results
-        mpnet_score = self.mpnet_classifier(mpnet_sequence)[0]['label']
-        if mpnet_score == 'correct_answer':
-            mpnet_res = True
-        elif mpnet_score == 'incorrect_answer':
-            mpnet_res = False
+        mpnet_score = self.mpnet_classifier(mpnet_sequence)[0]["label"]
+        if mpnet_score == "correct_answer":
+            mpnet_passing = True
+        elif mpnet_score == "incorrect_answer":
+            mpnet_passing = False
 
         # Majority voting
-        if mpnet_res == True and bleurt_res == True:
+        if bleurt_passing and mpnet_passing:
             return 2
-        elif mpnet_res == False and bleurt_res == False:
-            return 0
-        else:
+        elif bleurt_passing or mpnet_passing:
             return 1
+        else:
+            return 0
