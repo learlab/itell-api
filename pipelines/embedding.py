e9a4a7961089d8c1faf15c6bdd618ef72627de6e
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2023-10-02 13:07:26 -0500

    move retrieve_chunks logic to generate_embeddings

--- pipelines/embedding.py
+++ pipelines/embedding.py
@@ -1,8 +1,7 @@
 from sentence_transformers import SentenceTransformer
-from supabase.client import Client
 
 
-class EmbeddingPipeline(object):
+class EmbeddingPipeline:
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
     def __init__(self):
@@ -10,22 +9,3 @@ class EmbeddingPipeline(object):
 
     def __call__(self, text_input: str) -> list[float]:
         return self.model.encode(text_input).tolist()
-
-    def retrieve_chunks(
-        self,
-        text_input: str,
-        db: Client,
-        match_threshold: float = 0.3,
-        match_count: int = 3,
-    ) -> list[dict]:
-        embedding = self(text_input)
-        results = db.rpc(
-            "retrieve_chunks",
-            {
-                "embedding": embedding,
-                "match_threshold": match_threshold,
-                "match_count": match_count,
-            },
-        ).execute()
-
-        return results.data
