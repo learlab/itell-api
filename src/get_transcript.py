3d80f1dc26ee4bf8f6dff2cdf4c50a202ceaba95
Langdon <55119338+langdonholmes@users.noreply.github.com>
2023-11-08 18:04:40 +0000

    fix imports

--- src/get_transcript.py
+++ src/get_transcript.py
@@ -1,6 +1,6 @@
 from models.transcript import TranscriptInput, TranscriptResults
 from youtube_transcript_api import YouTubeTranscriptApi
-import os
+from fastapi import HTTPException
 from urllib import parse
 
 
@@ -9,30 +9,38 @@ class Transcript:
         self.url = transcript_input.url
         self.start_time = transcript_input.start_time
         self.end_time = transcript_input.end_time
-        self.valid_netlocs = ['www.youtube.com', 'youtu.be']
-    
+        self.valid_netlocs = ["www.youtube.com", "youtu.be"]
+
     def get_transcript(self):
         url = self.url
         url_parsed = parse.urlparse(url)
         netloc = url_parsed.netloc
-        assert netloc in self.valid_netlocs, f'{netloc} is not a valid location. Only YouTube video transcripts can be fetched.'
-        
+        assert (
+            netloc in self.valid_netlocs
+        ), f"{netloc} is not a valid location. Only YouTube video transcripts can be fetched."
+
         qsl = parse.parse_qs(url_parsed.query)
-        video_code = qsl['v'][0]
-        
+        video_code = qsl["v"][0]
+
         srt = YouTubeTranscriptApi.get_transcript(video_code)
 
         start_time = self.start_time or 0
-        end_time = self.end_time or srt[-1]['start']
+        end_time = self.end_time or srt[-1]["start"]
 
-        if start_time > srt[-1]['start']:
-            raise HTTPException(status_code=400, detail="Start time is beyond the range of the video")
+        if start_time > srt[-1]["start"]:
+            raise HTTPException(
+                status_code=400, detail="Start time is beyond the range of the video"
+            )
         if end_time < start_time:
-            raise HTTPException(status_code=400, detail="Start time must come before end time")
+            raise HTTPException(
+                status_code=400, detail="Start time must come before end time"
+            )
 
-        transcript_timeframe = [segment for segment in srt if end_time > segment['start'] >= start_time]                                    
-        transcript = ' '.join([x['text'] for x in transcript_timeframe])
-        
+        transcript_timeframe = [
+            segment for segment in srt if end_time > segment["start"] >= start_time
+        ]
+        transcript = " ".join([x["text"] for x in transcript_timeframe])
+
         return transcript
 
 
