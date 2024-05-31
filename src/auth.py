1906d8066d7c23c86a6f6c7f33d54cd5687fe76d
langdon <noreply@howdoiconfigurethisautomatically.com>
2024-05-31 18:12:11 +0000

    tweak auth logic organization

--- src/auth.py
+++ src/auth.py
@@ -5,6 +5,7 @@ from cachetools import cached, TTLCache
 
 api_key_header = APIKeyHeader(name="API-Key")
 
+
 @cached(cache=TTLCache(maxsize=1024, ttl=600))
 def get_role(api_key_header: str = Security(api_key_header)):
     if not api_key_header:
@@ -12,26 +13,29 @@ def get_role(api_key_header: str = Security(api_key_header)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Missing API key",
         )
-    
+
     db = get_vector_store()
 
     role = (
-        db.table("api_keys").select("role").eq("api_key", api_key_header).execute().data
+        db.table("api_keys").select("role").eq(
+            "api_key", api_key_header).execute().data
     )
 
-    if role:
-        return role[0]["role"]
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key"
-    )
+    if not role:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key"
+        )
+
+    return role[0]["role"]
 
 
 def developer_role(api_key_header: str = Security(api_key_header)):
     role = get_role(api_key_header)
 
-    if role == "developer":
-        return role
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="You do not have permission to access this resource",
-    )
+    if not role == "developer":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to access this resource",
+        )
+
+    return role
