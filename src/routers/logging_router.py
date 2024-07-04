baf13e05982fba5b4e50267089bcc33d7f2d351d
= <=>
2024-07-04 04:35:29 +0000

    performance timing

--- src/routers/logging_router.py
+++ src/routers/logging_router.py
@@ -1,17 +1,22 @@
 import asyncio
 import json
 import logging
+import time
 import uuid
 from typing import AsyncIterator, Callable
 
 from fastapi import BackgroundTasks, Request, Response
 from fastapi.responses import StreamingResponse
 from fastapi.routing import APIRoute
-from rich import print
 from starlette.background import BackgroundTask
 
 
 class LoggingStreamingResponse(StreamingResponse):
+    """A StreamingResponse that stores the first and last chunk of the stream.
+    Also includes a performance timer and
+    methods to access the first and last messages of the stream as dicts
+    """
+
     def __init__(self, content: AsyncIterator, *args, **kwargs):
         super().__init__(self.wrap_content(content), *args, **kwargs)
         self._is_done = asyncio.Event()
@@ -20,11 +25,18 @@ class LoggingStreamingResponse(StreamingResponse):
         self._first_chunk = await content.__anext__()
         yield self._first_chunk
 
+        # Time to first token
+        self.ttft = time.perf_counter() - self.start_time
+
         self._last_chunk = None
         async for chunk in content:
             self._last_chunk = chunk
             yield chunk
+
         self._is_done.set()
+
+        # Full process time
+        self.process_time = time.perf_counter() - self.start_time
 
     def parse_sse(self, chunk: str | bytes) -> str:
         if isinstance(chunk, bytes):
@@ -34,10 +46,7 @@ class LoggingStreamingResponse(StreamingResponse):
         try:
             return json.loads(chunk.split("\ndata: ")[1])
         except (IndexError, TypeError, json.JSONDecodeError) as e:
-            print("!" * 80)
-            print(e)
-            print(chunk)
-            print("!" * 80)
+            logging.error(f"Failed to parse SSE chunk: {chunk}. Error: {e}")
 
     @property
     def first_message(self):
@@ -58,14 +67,21 @@ class LoggingRoute(APIRoute):
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:
+            start_time = time.perf_counter()
             req_body = await request.body()
             response = await original_route_handler(request)
             existing_task = response.background
+            process_time = time.perf_counter() - start_time
 
             if isinstance(response, StreamingResponse):
                 response_body = None
+                # For streaming responses, the end time is calculated
+                # when the stream is finished, not here.
+                response.start_time = start_time
             else:
                 response_body = response.body.decode()
+                # Add processing time to response if not a streaming response
+                response.process_time = process_time
 
             file_log = BackgroundTask(self.file_log, req_body.decode(), response_body)
             db_log = BackgroundTask(self.db_log, request, response)
@@ -100,6 +116,9 @@ class LoggingRoute(APIRoute):
             # Will not exist for passing summaries at /score/summary/stairs
             if response.last_message:
                 response_body.update(response.last_message)
+            else:
+                # Do not record ttft if STAIRS was not triggered
+                response.ttft = None
 
         elif response.body:
             response_body = response.body
@@ -111,6 +130,9 @@ class LoggingRoute(APIRoute):
             "response_body": response_body,
             "status_code": response.status_code,
             "ip_address": request.client.host,
+            "process_time": response.process_time,
+            "ttft": response.ttft if hasattr(response, "ttft") else None,
         }
 
-        print(payload)
+        print(payload["api_endpoint"], payload["ttft"], payload["process_time"])
+        # await request.app.state.supabase.table("logs").insert(payload).execute()
