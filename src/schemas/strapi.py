e624a0da046288ac995f6870dae9f918993f687d
= <=>
2024-07-24 18:05:40 +0000

    change names to match strapi update

--- src/schemas/strapi.py
+++ src/schemas/strapi.py
@@ -16,33 +16,49 @@ class Chunk(BaseModel):
 
 
 class Page(BaseModel):
-    slug: str
+    Slug: str
     Title: str
     HasSummary: bool
 
 
-class Text(BaseModel):
-    slug: str
+class Volume(BaseModel):
+    Slug: str
     Title: str
     Owner: str
     Description: Optional[str] = None
 
 
 # Page Types
-
-
 class Content(Page):
     Content: list[Chunk]
 
     @field_validator("Content")
-    def page_must_have_text(cls, v):
+    def page_must_have_chunks(cls, v):
         if len(v) == 0:
             raise ValueError("Strapi returned 0 chunks for this page.")
 
         return v
 
 
-# Fancy Generic Types for Defining Pages Populated with Specific Values
+"""
+This is a bit more complex. We want to validate the following response structures:
+
+PageWithVolume
+├── data: List[PopulatedItem[ItemWithVolume]]
+│   ├── PopulatedItem
+│   │   ├── id: int
+│   │   └── attributes: ItemWithVolume
+│   │       └── Volume: Response[PopulatedItem[Volume]]
+│   │           ├── data: PopulatedItem[Volume]
+│   │           │   ├── id: int
+│   │           │   └── attributes: Volume
+
+PageWithChunks
+├── data: List[PopulatedItem[Content]]
+│   ├── PopulatedItem
+│   │   ├── id: int
+│   │   └── attributes: Content
+"""
 
 Populus = TypeVar("Populus")
 
@@ -57,12 +73,12 @@ class PopulatedItem(BaseModel, Generic[Populus]):
 
 
 # Constructed Types
-class _PageWithText(Page):
-    text: Response[PopulatedItem[Text]]
+class ItemWithVolume(Page):
+    Volume: Response[PopulatedItem[Volume]]
 
 
-class PageWithText(BaseModel):
-    data: list[PopulatedItem[_PageWithText]]
+class PageWithVolume(BaseModel):
+    data: list[PopulatedItem[ItemWithVolume]]
 
     @field_validator("data")
     @classmethod
